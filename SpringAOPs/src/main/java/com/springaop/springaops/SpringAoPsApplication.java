22b021a5d08d59b331ed1f364b0e63e087cd5a08
Aryan Sohani <aryan.112123@gmail.com>
2026-05-01 10:49:51 +0530

    @Before(Execute(* com.springaop.springaops.Service.*.*(..)))

---
+++ SpringAOPs/src/main/java/com/springaop/springaops/SpringAoPsApplication.java
@@ -0,0 +1,13 @@
+package com.springaop.springaops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAoPsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAoPsApplication.class, args);
+    }
+
+}
---
+++ SpringAOPs/src/test/java/com/springaop/springaops/SpringAoPsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springaop.springaops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAoPsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
