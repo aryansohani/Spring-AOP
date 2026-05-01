6610fb655720e6a192fca706f5c64199f37ecb8e
Aryan Sohani <aryan.112123@gmail.com>
2026-05-01 10:57:18 +0530

    removed

--- SpringAOP/src/main/java/com/springaop/springaop/AOP/LoggingAspect.java
+++ SpringAOP/src/main/java/com/springaop/springaop/AOP/LoggingAspect.java
@@ -1,15 +1,22 @@
 package com.springaop.springaop.AOP;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
+
+@Component
+@Aspect
 public class LoggingAspect {
 
     public static final Logger LOGGER= LoggerFactory.getLogger(LoggingAspect.class);
 
-    public void LogMethodCall()
-    {
-        LOGGER.info("method called");
+    @Before("execution(* com.springaop.springaop.service.*.*(..))")
+    public void logMethodCall(JoinPoint joinPoint) {
+        LOGGER.info("Method called: " + joinPoint.getSignature().getName());
     }
 
 
